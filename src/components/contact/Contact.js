87ef10411d2e7d1164190d7ae648444e5fda6739
Joshua Alvarez <joshuaialvarez06@gmail.com>
2021-11-08 22:56:02 -0800

    Form submission attempt 1

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './Contact.css';
 
 const Contact = () => {
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById('contactForm');
+    let formData = new FormData(myForm);
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log('Form successfully submitted'))
+      .catch((error) => alert(error));
+  };
 
   return (
     <section id="contact" className="contactSection">
@@ -15,27 +26,40 @@ const Contact = () => {
           </p>
         </div>
         <div className="contactForm">
-          <form onSubmit={handleSubmit}>
+          <form
+            id="contactForm"
+            name="contact"
+            netlify
+            method="POST"
+            data-netlify="true"
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="form-name" value="contact" />
             <div className="formSection">
               <label htmlFor="name" className="formLabel">
                 Name
               </label>
-              <input type="text" className="formInput" />
+              <input type="text" name="name" className="formInput" />
             </div>
             <div className="formSection">
               <label htmlFor="email" className="formLabel">
                 Email
               </label>
-              <input type="text" className="formInput" />
+              <input type="email" name="email" className="formInput" />
             </div>
             <div className="formSection">
               <label htmlFor="message" className="formLabel">
                 Message
               </label>
-              <textarea type="text" className="formInput" rows="6" />
+              <textarea
+                type="text"
+                name="message"
+                className="formInput"
+                rows="6"
+              />
             </div>
             <button className="submitBtn" type="submit">
-              SUBMIT
+              SEND
             </button>
           </form>
         </div>
