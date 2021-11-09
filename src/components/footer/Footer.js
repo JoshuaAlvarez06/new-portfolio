458a110ec38b49e9490392ee240292404acd8cc3
Joshua Alvarez <joshuaialvarez06@gmail.com>
2021-11-08 23:48:45 -0800

    Added links to footer socials

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,9 +12,23 @@ const Footer = () => {
         <div className="footerRight">
           <h2>SOCIALS</h2>
           <div className="footerSocials">
-            <i className="fab fa-linkedin"></i>
-            <i className="fab fa-github-square"></i>
-            <i className="fas fa-envelope-square"></i>
+            <a
+              href="https://www.linkedin.com/in/joshuaalvarez06"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              href="https://www.github.com/joshuaalvarez06"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github-square"></i>
+            </a>
+            <a href="mailto:joshuaialvarez06@gmail.com">
+              <i className="fas fa-envelope-square"></i>
+            </a>
           </div>
         </div>
       </div>
