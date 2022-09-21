71fd1353d709b25c2011dabf21859ac0f6f1a508
joshuaalvarez06 <joshuaialvarez06@gmail.com>
2022-09-20 23:56:49 -0700

    use react gtm library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_PUBLIC_GOOGLE_ANALYTICS,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +16,5 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
