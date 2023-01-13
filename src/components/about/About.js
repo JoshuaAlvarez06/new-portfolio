5277ee418a92f483e9df2813313d850e693a9966
joshuaalvarez06 <joshuaialvarez06@gmail.com>
2023-01-13 14:50:01 -0800

    dynamic age

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './About.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useMemo } from "react";
+import { Link } from "react-router-dom";
+import "./About.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
   AOS.init();
+
+  const age = useMemo(() => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    return currentMonth >= 2 ? currentYear - 2002 : currentYear - 2003;
+  }, []);
+
   return (
-    <section id='about' className='aboutSection'>
-      <div className='aboutContainer'>
-        <div className='aboutHeading'>
+    <section id="about" className="aboutSection">
+      <div className="aboutContainer">
+        <div className="aboutHeading">
           <h2>ABOUT ME</h2>
           <p>
             This is some basic information about me, more focused on the
             technological aspects of my life. Thanks for being here!
           </p>
         </div>
-        <div className='aboutContent'>
-          <div className='aboutLeft' data-aos='fade-right'>
-            <h3 className='contentHeading'>Get to know me</h3>
+        <div className="aboutContent">
+          <div className="aboutLeft" data-aos="fade-right">
+            <h3 className="contentHeading">Get to know me</h3>
             <p>
               I have been around technology all my life, and I have grown to
               have a deep love for it. Since beginning to code, it has become my
@@ -27,35 +34,35 @@ const About = () => {
               of knowledge.
             </p>
             <p>
-              As a 19 year old developer, I am excited to learn and add more to
-              my skill set every single day to become the best developer I can
-              be. Never stop learning!
+              As a {age} year old developer, I am excited to learn and add more
+              to my skill set every single day to become the best developer I
+              can be. Never stop learning!
             </p>
             <Link
-              className='contactBtn'
-              to='/#contact'
+              className="contactBtn"
+              to="/#contact"
               onClick={() => {
-                document.getElementById('contact').scrollIntoView({
-                  behavior: 'smooth',
+                document.getElementById("contact").scrollIntoView({
+                  behavior: "smooth",
                 });
               }}
             >
               CONTACT
             </Link>
           </div>
-          <div className='aboutRight' data-aos='fade-left'>
-            <h3 className='contentHeading'>Skills</h3>
-            <div className='skills'>
-              <div className='skill'>React</div>
-              <div className='skill'>Next.js</div>
-              <div className='skill'>JS / TS</div>
-              <div className='skill'>HTML</div>
-              <div className='skill'>CSS</div>
-              <div className='skill'>Git</div>
-              <div className='skill'>Node.js</div>
-              <div className='skill'>Express.js</div>
-              <div className='skill'>PostgreSQL</div>
-              <div className='skill'>Email Dev.</div>
+          <div className="aboutRight" data-aos="fade-left">
+            <h3 className="contentHeading">Skills</h3>
+            <div className="skills">
+              <div className="skill">React</div>
+              <div className="skill">Next.js</div>
+              <div className="skill">JS / TS</div>
+              <div className="skill">HTML</div>
+              <div className="skill">CSS</div>
+              <div className="skill">Git</div>
+              <div className="skill">Node.js</div>
+              <div className="skill">Express.js</div>
+              <div className="skill">PostgreSQL</div>
+              <div className="skill">Email Dev.</div>
             </div>
           </div>
         </div>
